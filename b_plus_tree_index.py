74737244a04aec3c4aeb391753da0f3d804a8017
alancheg <chenggui53@gmail.com>
2017-06-14 21:10:34 +0800

    Initial commit

---
+++ b_plus_tree_index.py
@@ -0,0 +1,19 @@
+# from binarytree import tree, pprint, inspect
+
+# 首先确定表数据的数据结构
+# 根据表数据的数据结构确定索引的方式
+
+import time
+		
+
+class BPlustreeIndex():
+	def __init__(self):
+		pass
+	
+
+if __name__ == "__main__":
+	
+	# 首先，所有的数据都是存储在 csv 文件中的
+	# 然后 对比索引的构建时间和查询时间的对比
+	
+	pass
